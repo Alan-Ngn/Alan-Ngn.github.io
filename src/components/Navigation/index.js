f7e55d311c4f814c8b32b0aca5de35a69b06bd00
Alan Nguyen <alanngn21@gmail.com>
2023-09-29 15:59:06 -0700

    added settimeout for textbox

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,6 +4,7 @@ import "./navigation.css";
 import React, { useState } from 'react';
 function Navigation(){
     const [transform, setTransform] = useState({ x: 20, y: 0})
+    const [chatStart, setChatStart] = useState("talk-bubble tri-right left-in")
 
     const handleStart = (e) => {
         const newTransform = {
@@ -11,6 +12,9 @@ function Navigation(){
           y: 0,
         };
         setTransform(newTransform);
+        setTimeout(()=> {
+          setChatStart("talk-bubble tri-right left-in")
+        },1000)
       };
     const handleDragonMove = (e) => {
         const newTransform = {
@@ -18,6 +22,7 @@ function Navigation(){
           y: -700,
         };
         setTransform(newTransform);
+        setChatStart("hide");
       };
 
       const handleWizardMove = (e) => {
@@ -26,14 +31,16 @@ function Navigation(){
           y: -300,
         };
         setTransform(newTransform);
+        setChatStart("hide");
       };
 
       const handleChestMove = (e) => {
         const newTransform = {
           x: 0,
-          y: -850,
+          y: -820,
         };
         setTransform(newTransform);
+        setChatStart("hide");
       };
 
       const handleKnightMove = (e) => {
@@ -42,9 +49,21 @@ function Navigation(){
           y: -450,
         };
         setTransform(newTransform);
+        setChatStart("hide");
+      };
+      const handleScrollMove = (e) => {
+        const newTransform = {
+          x: -50,
+          y: -325,
+        };
+
+        setTransform(newTransform);
+        setChatStart("hide");
+
+
       };
 
-      const chatClassName = "talk-bubble tri-right left-in" + (transform.x !== 20 || transform.y !== 0 ? " hide" : "")
+      // const chatClassName = "talk-bubble tri-right left-in" + (transform.x !== 20 || transform.y !== 0 ? " hide" : "")
     return (
         <>
             <img className="me-avatar" src={process.env.PUBLIC_URL + '/me.png'}
@@ -52,7 +71,7 @@ function Navigation(){
                     transform: `translate(${transform.x}px, ${transform.y}px)`
                 }}
             />
-            <div className={chatClassName}>
+            <div className={chatStart}>
               <div className="talktext">
 
               <p>
@@ -77,6 +96,10 @@ function Navigation(){
             <button onClick={handleKnightMove}>
               <img  className='knight' src={process.env.PUBLIC_URL + '/knight.png'}/>
               Knight
+            </button>
+            <button onClick={handleScrollMove}>
+              <img  className='scroll' src={process.env.PUBLIC_URL + '/scroll.png'}/>
+              Scroll
             </button>
             <button
                 onClick={handleStart}
