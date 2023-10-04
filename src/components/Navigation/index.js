08fcf66f9d7bb6b26a8d42a8917a31a6970801a8
Alan Nguyen <alanngn21@gmail.com>
2023-10-04 02:07:55 -0700

    working on about me

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -26,61 +26,9 @@ function Navigation(){
           setStart("talk-bubble tri-right left-in")
         },1000)
       };
-    const handleDragonMove = (e) => {
-        const newTransform = {
-          x: 40,
-          y: -700,
-        };
-        setTransform(newTransform);
-        setStart("hide");
-      };
 
-      const handleWizardMove = (e) => {
-        const newTransform = {
-          x: -240,
-          y: -300,
-        };
-        setTransform(newTransform);
-        setStart("hide");
-      };
-
-      const handleChestMove = (e) => {
-        const newTransform = {
-          x: 0,
-          y: -820,
-        };
-        setTransform(newTransform);
-        setStart("hide");
-      };
-
-      const handleKnightMove = (e) => {
-        const newTransform = {
-          x: 10,
-          y: -450,
-        };
-        setTransform(newTransform);
-        setStart("hide");
-      };
-      const handleScrollMove = (e) => {
-        const newTransform = {
-          x: 10,
-          y: 10,
-        };
-
-        setTransform(newTransform);
-        setStart("hide");
-
-
-      };
-
-      // const chatClassName = "talk-bubble tri-right left-in" + (transform.x !== 20 || transform.y !== 0 ? " hide" : "")
     return (
         <>
-
-
-
-
-
               <div className={start}>
                 <div className="talktext">
                   <p>
@@ -96,30 +44,6 @@ function Navigation(){
                       transform: `translate(${transform.x}px, ${transform.y}px)`
                   }}
               />
-              {/* <button onClick={handleWizardMove}>
-                <img  className='wizard' src={process.env.PUBLIC_URL + '/wizard.png'}/>
-                Wizard
-              </button> */}
-{/*
-              <button onClick={handleDragonMove}>
-                  <img  className='dragon' src={process.env.PUBLIC_URL + '/dragon.png'}/>
-                Dragon
-              </button>
-
-              <button onClick={handleChestMove}>
-                <img  className='chest' src={process.env.PUBLIC_URL + '/chest.png'}/>
-                Chest
-              </button>
-
-              <button onClick={handleKnightMove}>
-              <img  className='knight' src={process.env.PUBLIC_URL + '/knight.png'}/>
-              Knight
-            </button> */}
-
-            {/* <button onClick={handleScrollMove}>
-              <img  className='scroll' src={process.env.PUBLIC_URL + '/scroll.png'}/>
-              Scroll
-            </button> */}
 
             <button
                 onClick={handleStart}
