8c325bcf15a6ea79918670527f5c77480104b670
Alan Nguyen <alanngn21@gmail.com>
2023-10-15 18:46:32 -0700

    changed button for project modals

--- src/components/KnightModal/index.js
+++ src/components/KnightModal/index.js
@@ -1,12 +1,24 @@
+
 const KnightModal = () => {
+    const openLiveLink = () => {
+        window.open("https://mango-music-4c4k.onrender.com", "_blank")
+    }
+    const openGithub = () => {
+        window.open("https://github.com/Mango-WorldWide/Mango-Music", "_blank")
+    }
     return (
             <article class="Project">
                 <img className='MangoSplash' src={process.env.PUBLIC_URL + '/MangoMusicThumb.PNG'}/>
                 <h3>Mango Music</h3>
                 <p>Mango Music draws inspiration from Apple Music and employs JavaScript, React, Redux, Python, Flask, SQLAlchemy, AWS S3, HTML, and CSS. It features a persistent global player, and an integration with AWS S3 for storing and serving .mp4 files.</p>
                 <div className="site-links">
-                    <a href="https://mango-music-4c4k.onrender.com" target="_blank" class="button">Live Link</a>
-                    <a href="https://github.com/Mango-WorldWide/Mango-Music" target="_blank" class="button">Github</a>
+                    <button onClick={openLiveLink}>
+                        Live Link
+                    </button>
+                    <button onClick={openGithub}>
+                        Github
+                    </button>
+
                 </div>
             </article>
     )
