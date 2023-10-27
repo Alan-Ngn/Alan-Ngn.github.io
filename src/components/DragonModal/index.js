6f7d7bb109d7bea53f61d82f0feea78754ad8a4e
Alan Nguyen <alanngn21@gmail.com>
2023-10-26 21:17:18 -0700

    moved earthbnb links on to image

--- src/components/DragonModal/index.js
+++ src/components/DragonModal/index.js
@@ -11,23 +11,33 @@ const DragonModal = () => {
         window.open("https://github.com/Alan-Ngn/API-project", "_blank")
     }
     return (
-        
+
 
             <article class="Project">
                 <button className="close-button" onClick={closeModal}>
                     <i class="fa-regular fa-rectangle-xmark"></i>
                 </button>
-                <img className='EarthBnBSplash' src={process.env.PUBLIC_URL + '/EarthbnbSplashPage.PNG'}/>
+                <div className="earth-image-link">
+                    <img className='EarthBnBSplash' src={process.env.PUBLIC_URL + '/EarthbnbSplashPage.PNG'}/>
+                    <div className="link-github-icon">
+                        <a href="https://earthbnb-xzrt.onrender.com" rel="noreferrer" target="_blank">
+                            <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                        </a>
+                        <a href="https://github.com/Alan-Ngn/API-project" rel="noreferrer" target="_blank">
+                            <i className="fa-brands fa-github" />
+                        </a>
+				    </div>
+                </div>
                 <h3>EarthBnB</h3>
                 <p>EarthBnB is a JavaScript, React, Redux, and Express-based clone of Airbnb. It achieves dynamic mapping of vacation rentals on the splash page and enhances scalability through Object-Oriented Programming (OOP) and component-based architecture.</p>
-                <div className="site-links">
+                {/* <div className="site-links">
                     <button className="project-button" onClick={openLiveLink}>
                         Live Link
                     </button>
                     <button className="project-button" onClick={openGithub}>
                         Github
                     </button>
-                </div>
+                </div> */}
             </article>
 
 
