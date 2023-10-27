5e6d7c0fe125bf2c2a27b5049eae2eaf485121a1
Alan Nguyen <alanngn21@gmail.com>
2023-10-26 23:24:24 -0700

    added link to projects images

--- src/components/DragonModal/index.js
+++ src/components/DragonModal/index.js
@@ -17,7 +17,7 @@ const DragonModal = () => {
                 <button className="close-button" onClick={closeModal}>
                     <i class="fa-regular fa-rectangle-xmark"></i>
                 </button>
-                <div className="earth-image-link">
+                <div className="image-link">
                     <img className='EarthBnBSplash' src={process.env.PUBLIC_URL + '/EarthbnbSplashPage.PNG'}/>
                     <div className="link-github-icon">
                         <a href="https://earthbnb-xzrt.onrender.com" rel="noreferrer" target="_blank">
@@ -38,6 +38,26 @@ const DragonModal = () => {
                         Github
                     </button>
                 </div> */}
+                <h3>Skills</h3>
+                    <div class="skills-img">
+
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" />
+
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" />
+
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
+
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
+
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
+
+                    </div>
             </article>
 
 
--- src/components/KnightModal/index.js
+++ src/components/KnightModal/index.js
@@ -13,10 +13,21 @@ const KnightModal = () => {
                 <button className="close-button" onClick={closeModal}>
                     <i class="fa-regular fa-rectangle-xmark"></i>
                 </button>
-                <img className='MangoSplash' src={process.env.PUBLIC_URL + '/MangoMusicThumb.PNG'}/>
+                <div className="image-link">
+                    <img className='MangoSplash' src={process.env.PUBLIC_URL + '/MangoMusicThumb.PNG'}/>
+                    <div className="link-github-icon">
+                        <a href="https://mango-music-4c4k.onrender.com" rel="noreferrer" target="_blank">
+                            <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                        </a>
+                        <a href="https://github.com/Mango-WorldWide/Mango-Music" rel="noreferrer" target="_blank">
+                            <i className="fa-brands fa-github" />
+                        </a>
+				    </div>
+                </div>
+
                 <h3>Mango Music</h3>
                 <p>Mango Music draws inspiration from Apple Music and employs JavaScript, React, Redux, Python, Flask, SQLAlchemy, AWS S3, HTML, and CSS. It features a persistent global player, and an integration with AWS S3 for storing and serving .mp4 files.</p>
-                <div className="site-links">
+                {/* <div className="site-links">
                     <button className="project-button" onClick={openLiveLink}>
                         Live Link
                     </button>
@@ -24,6 +35,29 @@ const KnightModal = () => {
                         Github
                     </button>
 
+                </div> */}
+                <h3>Skills</h3>
+                <div class="skills-img">
+
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
+
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" />
+
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
+
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
+
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
+
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlalchemy/sqlalchemy-original-wordmark.svg" />
+
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" />
                 </div>
             </article>
     )
